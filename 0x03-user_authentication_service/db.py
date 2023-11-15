cd10d25fedf34b54bf2f6d5b79beae08e366e461
peter njihia <peternjihia2001@gmail.com>
2023-11-15 16:42:10 +0000

    find user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" db class orm"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base, User
+
+
+class DB:
+    """ db class """
+    def __init__(self):
+        """ constructor """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """ Session getter """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Adds and returns a new user Object """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)  # add the new user
+        self._session.commit()  # update the DataBase
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Function that finds a created user
+        using a argument """
+        if kwargs is None:
+            raise InvalidRequestError
+
+        for i in kwargs.keys():
+            if i not in User.__table__.columns.keys():
+                raise InvalidRequestError
+
+        user_query = self._session.query(User).filter_by(**kwargs).first()
+
+        if user_query is None:
+            raise NoResultFound
+
+        return user_query
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Update properties of an user """
+        user = self.find_user_by(id=user_id)
+        for i in kwargs.keys():
+            if i not in User.__table__.columns.keys():
+                raise ValueError
+
+        for k, v in kwargs.items():
+            setattr(user, k, v)
+
+        self._session.commit()
